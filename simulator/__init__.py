5e70996f8e2a4336665c200a2af4c9c1eac7d696
Ashot Matevosyan <ashmat98@gmail.com>
2023-02-07 23:23:52 +0100

    for shorter imports

---
+++ simulator/__init__.py
@@ -0,0 +1,7 @@
+from simulator.magnetic import SimulatorMagnetic
+from simulator.lennard import SimulatorLennard
+from simulator.ideal import SimulatorIdeal
+from simulator.base import SimulatorBase
+from simulator.baseIO import SimulatorBaseIO
+
+from simulator.models import Simulation, Client, Client_HDF5
